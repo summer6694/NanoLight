7713c306a446bbcbf91590998c2cae0f7f9617d6
Summer <35442185+summer6694@users.noreply.github.com>
2025-09-20 13:28:10 +0800

    解决只能访问index.html无法访问js及css资源文件的问题
    解决只能访问index.html无法访问js及css资源文件的问题

--- nano_core/src/web_server.cpp
+++ nano_core/src/web_server.cpp
@@ -37,10 +37,20 @@ bool NanoServer::server_dir(fs::FS& fs) {
     File file = root.openNextFile();
     auto folder_len = strlen(WEB_ROOT);
     server_.serveStatic("/", fs, "/web/index.html");
+    // 解决只能访问index.html无法访问js及css资源文件的问题
+    // server_.serveStatic("/main.816ac712.css", fs, "/web/main.816ac712.css");
+    // server_.serveStatic("/main.b11ab86c.js", fs, "/web/main.b11ab86c.js");
     while (file) {
         if (!file.isDirectory()) {
             auto filename = file.name();
-            server_.serveStatic(filename + folder_len, fs, filename);
+            auto filename_len = strlen(filename);
+            char urlPath[filename_len + 2]; // 1 for '/', 1 for '\0'
+            char filePath[filename_len + 2 + folder_len]; // 1 for '/', 1 for '\0'
+            snprintf(urlPath, sizeof(urlPath), "/%s", filename);
+            snprintf(filePath, sizeof(filePath), "%s/%s", WEB_ROOT, filename);
+            // 正确拼接路径
+            server_.serveStatic(urlPath, fs, filePath);
+            //server_.serveStatic(filename + folder_len, fs, filename);
         }
         file = root.openNextFile();
     }
